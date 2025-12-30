33541bfd734cd2154cec9b4e1dce9a5ecdf93980
RogerJackD <rogerzapata114@gmail.com>
2025-12-29 22:24:14 -0500

    feat(dto): add validator fields createProduct & updateProduct

--- src/catalog/dto/create-product.dto.ts
+++ src/catalog/dto/create-product.dto.ts
@@ -1,7 +1,19 @@
+import { IsNumber, IsOptional, IsString } from "class-validator";
+
 export class CreateProductDto {
+    @IsString()
     codigoMercaderia: string;
+
+    @IsString()
     nombre: string;
+
+    @IsNumber()
     FamiliaProducto: number;
+
+    @IsNumber()
     precio: number;
+
+    @IsString()
+    @IsOptional()
     foto: string;
 }
--- src/catalog/dto/update-product.dto.ts
+++ src/catalog/dto/update-product.dto.ts
@@ -1,6 +1,12 @@
+import { IsOptional } from "class-validator";
+
 export class UpdateProductDto {
+    @IsOptional()
     nombre?: string;
+    @IsOptional()
     FamiliaProducto?: number;
+    @IsOptional()
     precio?: number;
+    @IsOptional()
     foto?: string;
 }
