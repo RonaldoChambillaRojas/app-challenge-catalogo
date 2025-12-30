47ca097d28e42e93292de86c4b068d1a79caa3da
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-12-29 23:23:16 -0500

    feat: add pagination dto

---
+++ src/catalog/dto/pagination.dto.ts
@@ -0,0 +1,14 @@
+import { IsOptional, IsNumberString, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class PaginationDto {
+  @IsOptional()
+  @Type(() => Number)
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @Min(1)
+  limit?: number = 10; // Valor por defecto: 10 productos por página
+}
